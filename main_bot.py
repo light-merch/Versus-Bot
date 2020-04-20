607a3e62c36adc8b1c63288267f546c705851bbb
pavTiger <pavTiger@gmail.com>
2020-04-20 11:40:22 +0300

    Discord Bot

---
+++ main_bot.py
@@ -0,0 +1,22 @@
+import discord
+from my_token import discord_token 
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if (message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('--' * 10)
+
+client.run(discord_token)
--- my_token.py
+++ my_token.py
@@ -1,1 +1,3 @@
 token = 'kWrLl_Jhme8JfwqmnE8HoVZ_mEV4RrDHFwLfznI75_yWZDfnr3kJGcbfD1nllqb9'
+
+discord_token = ''

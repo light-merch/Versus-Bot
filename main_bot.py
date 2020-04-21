baf72509862f65da0951b366a05e749e43bcccc3
dprozz <dmitri.proskuriakov@gmail.com>
2020-04-21 10:24:38 +0300

    added save user data function

--- main_bot.py
+++ main_bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 
+import json
 import chain as ch
 from my_token import discord_token 
 from battle_parser import get_lyrics
@@ -9,17 +10,27 @@ from battle_parser import get_lyrics
 # TODO add explict
 # TODO save to file
 EXPLICT = dict() # show explict content
-REGISTERED = []  # list of joined people
+REGISTERED = {}  # list of joined people
 RAPPERS = []     # all rappers parsed
 
 def test(user_id):
     return user_id in REGISTERED
 
-
-def read_data():
+def read_rappers():
     with open('artists.txt', 'r') as f:
         RAPPERS = f.read().split(';')
-
+        
+def write_rappers():
+    with open('artists.txt', 'w') as f:
+        f.write(rappers)
+        
+def read_users():
+    with open('user_data.json', 'r') as f:
+        REGISTERED = json.load(f)
+        
+def write_users():
+    with open('user_data.json', 'w') as f:
+        json.dump(REGISTERED, f)
 
 client = discord.Client()
 bot = commands.Bot(command_prefix='vs ')
@@ -28,7 +39,8 @@ bot = commands.Bot(command_prefix='vs ')
 @bot.event
 async def on_ready():
     print('Started!')
-    read_data()
+    read_rappers()
+    read_users()
     game = discord.Game("GTA Online")
     await bot.change_presence(status=discord.Status.online, activity=game)
 
@@ -38,9 +50,9 @@ async def start(ctx):
     if ctx.author.id in REGISTERED:
         await ctx.send('Already registed')
         return
-
-    EXPLICT[ctx.author.id] = False
-    REGISTERED.append(ctx.author.id)
+    REGISTERED[ctx.author.id] = {}
+    REGISTERED[ctx.author.id]["EXPLICT"] = False
+    write_users()
 
     await ctx.send('Registed')
 
@@ -69,10 +81,10 @@ async def add(ctx, arg):
     if test(ctx.author.id):
         if arg in RAPPERS:
             await ctx.send('There is already this rapper in our dataset. See <vs rappers> for more')
-
         await ctx.send('Started parsing songs')
         get_lyrics(arg)
         RAPPERS.append(arg)
+        write_rappers()
         await ctx.send('Done. See <vs rappers> to choose new rapper')
     else:
         await ctx.send('Please register by typing <vs start>')
@@ -81,12 +93,9 @@ async def add(ctx, arg):
 @bot.command(pass_context=True)
 async def explict(ctx):
     if test(ctx.author.id):
-        if EXPLICT[ctx.author.id]:
-            EXPLICT[ctx.author.id] = False
-        else:
-            EXPLICT[ctx.author.id] = True
-
-        ans = f'Explict content set to ' + ['show', 'hide'][not EXPLICT[ctx.author.id]]
+        REGISTERED[ctx.author.id]["EXPLICT"] = !REGISTERED[ctx.author.id]["EXPLICT"]
+        write_users()
+        ans = f'Explict content set to ' + ['show', 'hide'][!REGISTERED[ctx.author.id]["EXPLICT"]]
         await ctx.send(ans)
     else:
         await ctx.send('Please register by typing <vs start>')
